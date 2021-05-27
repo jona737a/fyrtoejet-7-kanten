97a00966313d9d707a887176d1b95d80a74d0ee5
jona737a <jonathansmadsen@gmail.com>
2021-05-27 13:07:24 +0200

    Sidste rettelser efter test

--- src/views/Camera.vue
+++ src/views/Camera.vue
@@ -3,6 +3,16 @@
     <p class="error"></p>
     <qrcode-stream class="live" @decode="onDecode" @init="onInit" />
     <img class="kejser" src="https://firebasestorage.googleapis.com/v0/b/fyrtoejet-eb6bc.appspot.com/o/Kejser.svg?alt=media&token=e3e281b4-826c-42f2-8320-5e180dede9c7" alt="Kejser">
+    <div class="done" v-if="this.alert == true">
+        <div class="resultBox">
+            <div>
+                <h2>Allerede besvaret</h2>
+                <p>Du har allerede svaret på dette spørgsmål</p>
+                <p>Find en anden og scan igen</p>
+            </div>
+            <v-btn @click="$router.push('/')">Gå videre</v-btn>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -20,7 +30,8 @@ export default {
   },
   data () {
     return {
-      error: ''
+      error: '',
+      alert: false,
     }
   },
   methods: {
@@ -38,7 +49,7 @@ export default {
           this.$store.commit('currentQuestUpdate', code)
           this.$router.push('/spoergsmaal');
         } else {
-          console.log('Already answered')
+          this.alert = true
         }
     },
     onDecode (result) {
@@ -104,5 +115,33 @@ export default {
 .kejser{
   width: 75vw;
   align-self: flex-end;
+}
+
+.alert{
+    position: fixed;
+    top: 0;
+    background-color: rgba(0, 0, 0, 0.432);
+    height: 100%;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 2;
+    .resultBox{
+        background-color: colors(white);
+        height: 100vw;
+        width: 100vw;
+        box-shadow: shadow();
+        display: flex;
+        flex-flow: column;
+        justify-content: space-evenly;
+        align-items: center;
+        p{
+            font-size: 5vw;
+            margin-top: 3vw;
+            width: 80vw;
+        }
+        
+    }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,6 +15,16 @@
       <img src="https://firebasestorage.googleapis.com/v0/b/fyrtoejet-eb6bc.appspot.com/o/Soldat.svg?alt=media&token=4e2bf79f-e9b5-4ee9-b23f-eaf95d804a5e" alt="Soldat">
       <img src="https://firebasestorage.googleapis.com/v0/b/fyrtoejet-eb6bc.appspot.com/o/Heks.svg?alt=media&token=e9869b7e-ad7f-4c3d-966d-2050730d8ebe" alt="Heks">
     </div>
+    <div class="done" v-if="this.answered >= this.totalQuest">
+        <div class="resultBox">
+            <div>
+                <h2>Tillykke</h2>
+                <p>Du har svaret på alle spørgsmål</p>
+                <p>Åben pointtavlen for at se om du har vundet</p>
+            </div>
+            <v-btn @click="$router.push('/pointtavle')">Gå videre</v-btn>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -70,5 +80,32 @@ export default {
   img{
     width: 30vw;
   }
+}
+
+.done{
+    position: fixed;
+    top: 0;
+    background-color: rgba(0, 0, 0, 0.432);
+    height: 100%;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 2;
+    .resultBox{
+        background-color: colors(white);
+        height: 100vw;
+        width: 100vw;
+        box-shadow: shadow();
+        display: flex;
+        flex-flow: column;
+        justify-content: space-evenly;
+        align-items: center;
+        p{
+            font-size: 5vw;
+            margin-top: 3vw;
+            width: 80vw;
+        }
+    }
 }
 </style>
